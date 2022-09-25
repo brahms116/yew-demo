70914979a879477d90c22e78f0a26806904c6a0b
David Kwong <20544dk@gmail.com>
2022-09-25 00:56:55 +0000

    main - demo

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use yew::{function_component, html, use_state, Callback};
+
+#[function_component(App)]
+fn app() -> Html {
+    let counter = use_state(|| 0);
+
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "Increment value" }</button>
+            <p>
+                <b>{ "Current value: " }</b>
+                { *counter }
+            </p>
+
+        </div>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
